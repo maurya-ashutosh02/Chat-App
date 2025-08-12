b5e50ebac65c18b372267964b88aad76255da367
Ashutosh Maurya <ashutoshmaurya9696@gmail.com>
2025-08-12 15:41:13 +0530

    Chat-Application Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,13 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+
 app.use("/app/auth", authRoutes);
 app.use("/app/messages", messageRoutes);
+
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
